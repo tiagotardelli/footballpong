2073070254d0fc9fcb6f7b138f3b600f7af077a5
Tiago Tardelli <tiagob.tardelli@gmail.com>
2022-08-22 21:51:34 -0300

    Adicionado personagens e bolinha close #4

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from tkinter.tix import Balloon
 import pygame as pg
 
 pg.init()
 
 window = pg.display.set_mode([1280,720])
 title = pg.display.set_caption("Football Pong")
+
 field = pg.image.load("assets/field.png")
 window.blit(field,(0,0))
+
+player1 = pg.image.load("assets/player1.png")
+window.blit(player1, (50,310))
+
+player2 = pg.image.load("assets/player2.png")
+window.blit(player2, (1150,310))
+
+ball = pg.image.load("assets/ball.png")
+window.blit(ball, (617,337))
 
 loop = True
 while loop:
