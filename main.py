115e3fb066e5d7ff32e48c1df51bce0a638545db
Tiago Tardelli <tiagob.tardelli@gmail.com>
2022-08-25 03:21:30 -0300

    Movimento player 1 | close #11

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ title = pg.display.set_caption("Football Pong")
 field = pg.image.load("assets/field.png")
 
 player1 = pg.image.load("assets/player1.png")
+player1_y = 310
 
 player2 = pg.image.load("assets/player2.png")
 
@@ -15,7 +16,7 @@ ball = pg.image.load("assets/ball.png")
 ball_xy = {"x": 617, "y": 337}
 
 
-def move_ball(none):
+def move_ball():
     global ball_xy
 
     ball_xy["x"] += 1
@@ -23,7 +24,7 @@ def move_ball(none):
 
 def draw():
     window.blit(field, (0, 0))
-    window.blit(player1, (50, 310))
+    window.blit(player1, (50, player1_y))
     window.blit(player2, (1150, 310))
     window.blit(ball, (ball_xy["x"], ball_xy["y"]))
 
@@ -34,6 +35,11 @@ while loop:
     for events in pg.event.get():
         if events.type == pg.QUIT:
             loop = False
+        if events.type == pg.KEYDOWN:
+            if events.key == pg.K_w:
+                player1_y -= 10
+            if events.key == pg.K_s:
+                player1_y += 10
 
     draw()
     move_ball()
