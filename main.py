ddb124518fc12a223f1033930a3f7605878d3721
Tiago Tardelli <tiagob.tardelli@gmail.com>
2022-08-27 05:49:00 -0300

    close #19 #20 #21 #22 #23

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ pg.init()
 window = pg.display.set_mode([1280, 720])
 title = pg.display.set_caption("Football Pong")
 
+win = pg.image.load("assets/win.png")
+
+score1 = 0
+score1_img = pg.image.load("assets/score/0.png")
+score2 = 0
+score2_img = pg.image.load("assets/score/0.png")
+
 field = pg.image.load("assets/field.png")
 
 player1 = pg.image.load("assets/player1.png")
@@ -14,10 +21,13 @@ player1_movedown = False
 
 
 player2 = pg.image.load("assets/player2.png")
+player2_y = 310
 
 ball = pg.image.load("assets/ball.png")
 ball_x = 617
 ball_y = 337
+ball_dir = -3
+ball_dir_y = 1
 
 
 def move_player():
@@ -39,17 +49,65 @@ def move_player():
         player1_y = 575
 
 
-def move_ball():
-    global ball_x
+def move_player2():
+    global player2_y
+    player2_y = ball_y
 
-    ball_x += 1
+
+def move_ball():
+    global ball_x, ball_y
+    global ball_dir, ball_dir_y
+    global score1, score2
+    global score1_img, score2_img
+
+    ball_x += ball_dir
+    ball_y += ball_dir_y
+
+    if ball_x < 120:
+        if player1_y < ball_y + 23:
+            if player1_y + 146 > ball_y:
+                ball_dir *= -1
+
+    if ball_x > 1100:
+        if player2_y < ball_y + 23:
+            if player2_y + 146 > ball_y:
+                ball_dir *= -1
+
+    if ball_y > 700:
+        ball_dir_y *= -1
+    elif ball_y <= 0:
+        ball_dir_y *= -1
+
+    if ball_x <= -50:
+        ball_x = 617
+        ball_y = 337
+        ball_dir_y *= -1
+        ball_dir *= 1
+        score2 += 1
+        score2_img = pg.image.load(f"assets/score/{str(score2)}.png")
+
+    elif ball_x > 1320:
+        ball_x = 617
+        ball_y = 337
+        ball_dir_y *= -1
+        ball_dir *= 1
+        score1 += 1
+        score1_img = pg.image.load(f"assets/score/{str(score1)}.png")
 
 
 def draw():
-    window.blit(field, (0, 0))
-    window.blit(player1, (50, player1_y))
-    window.blit(player2, (1150, 310))
-    window.blit(ball, (ball_x, ball_y))
+    if score1 or score2 < 9:
+        window.blit(field, (0, 0))
+        window.blit(player1, (50, player1_y))
+        window.blit(player2, (1150, player2_y))
+        window.blit(ball, (ball_x, ball_y))
+        window.blit(score1_img, (500, 50))
+        window.blit(score2_img, (710, 50))
+        move_ball()
+        move_player()
+        move_player2()
+    else:
+        window.blit(win, (300, 330))
 
 
 loop = True
@@ -70,6 +128,5 @@ while loop:
                 player1_movedown = False
 
     draw()
-    move_ball()
-    move_player()
+
     pg.display.update()
