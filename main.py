3cba37fa8d8cc1f6f87dc5395f46dbf1115fa482
Tiago Tardelli <tiagob.tardelli@gmail.com>
2022-08-27 04:05:17 -0300

    Criado limite para o player 1 | close #17

--- main.py
+++ main.py
@@ -33,6 +33,11 @@ def move_player():
     else:
         player1_y += 0
 
+    if player1_y <= 0:
+        player1_y = 0
+    elif player1_y >= 575:
+        player1_y = 575
+
 
 def move_ball():
     global ball_x
